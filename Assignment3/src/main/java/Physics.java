c3ac28c2e62792fce99f6f893615fb9a7af91e9c
scaventum <scaventum@scavensys>
2017-10-06 14:01:36 +1300

    Initial commit

---
+++ Assignment3/src/main/java/Physics.java
@@ -0,0 +1,13 @@
+
+public class Physics {
+	public int force (int m, int a) {
+		return m * a;
+	}
+	public double time (int d, int s) {
+		return (double) d / s;
+	}
+	public int weight (int m) {
+		return m * 10;
+	}
+
+}
---
+++ Assignment3/src/test/java/PhysicsTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PhysicsTest {
+	int m, a, d, s; 
+	Physics obj;
+
+	@Before
+	public void setUp() throws Exception {
+		obj = new Physics();
+		m = 30;
+		a = 120;
+		d = 15;
+		s = 50;
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		obj = null;
+		m = 0;
+		a = 0;	
+		d = 0;
+		s = 0;
+	}
+
+	@Test
+	public void testForce() {
+		assertEquals (3600,obj.force(m,a));	
+	}
+
+	@Test
+	public void testTime() {
+		assertEquals (0.3,obj.time(d,s),0.1);	
+	}
+
+	@Test
+	public void testWeight() {
+		assertEquals (300,obj.weight(m));	
+	}
+
+}
